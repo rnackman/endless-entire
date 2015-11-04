e8cad77d1d50ea6e576cc6c0d5a21edccd66f7ee
rnackman <rachel.nackman@gmail.com>
2015-11-04 17:38:47 -0500

    Unstyled intro panel and dummy exhibition content.

--- myApp.js
+++ myApp.js
@@ -1,6 +1,8 @@
 var app = angular.module('myApp', []);
 
 app.run(function($rootScope) {
+  $rootScope.introShow = true;
+  $rootScope.exhShow = false;
   $rootScope.exh = {
     title: "Endless, Entire",
     subtitle: "American Abstract Artists and the Circle",
@@ -8,12 +10,79 @@ app.run(function($rootScope) {
     location: "FiveMyles",
     locationUrl: "http://www.fivemyles.org",
     curator: "Rachel Nackman"
-  }
+  };
+  $rootScope.toggleIntro = function(){
+    $rootScope.introShow = $rootScope.introShow == true ? false : true;
+    $rootScope.exhShow = true;
+  };
 });
 
-app.directive("introPanel", function(){
-  return {
-    restrict: 'E',
-    templateUrl: 'intro-panel.html'
-  };
+app.controller('ExhibitionController', function($scope){
+  $scope.artists = [
+    {
+      name: "Emily Berger",
+      artworks: [
+        {
+          title: "Painting 1",
+          date: 1985,
+          medium: "oil on paper",
+          imgsrc: 'images/Hsiao.jpg'
+        },
+        {
+          title: "Painting 2",
+          date: 1992,
+          medium: "oil on canvas"
+        }
+      ],
+      nationality: "American"
+    },
+    {
+      name: "Creighton Michael",
+      artworks: [
+        {
+          title: "Painting 3",
+          date: 1985,
+          medium: "oil on paper"
+        },
+        {
+          title: "Painting 5",
+          date: 1992,
+          medium: "oil on canvas"
+        }
+      ],
+      nationality: "American"
+    },
+    {
+      name: "Vera Vasek",
+      artworks: [
+        {
+          title: "Drawing 8",
+          date: 1985,
+          medium: "oil on paper"
+        },
+        {
+          title: "Painting 2",
+          date: 1992,
+          medium: "oil on canvas"
+        }
+      ],
+      nationality: "American"
+    },
+    {
+      name: "Anne Rusinoff",
+      artworks: [
+        {
+          title: "Drawing 1",
+          date: 1985,
+          medium: "oil on paper"
+        },
+        {
+          title: "Drawing 2",
+          date: 1992,
+          medium: "oil on canvas"
+        }
+      ],
+      nationality: "American"
+    }
+  ];
 });
